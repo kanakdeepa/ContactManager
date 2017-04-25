c920d7adcace7703211fb8594adeb644dde7232e
lalit-wittyparrot <lalit123>
2017-04-25 17:25:18 +0530

    content updated

---
+++ app/userService.js
@@ -0,0 +1,32 @@
+"use strict" ;
+
+angular.module('users')
+	.service('userService',UserService);
+
+
+	function UserService($q){
+
+		var users =[
+
+			{
+				name :"x",
+				avatar:'',
+				content :"XAMPP has been around for more than 10 years – there is a huge community behind it. You can get involved by joining our Forums."
+
+			},
+
+			{
+				name :"y",
+				avatar:'',
+				content :XAMPP has been around for more than 10 years – there is a huge community behind it. You can get involved by joining our Forums.
+
+			},
+
+
+			{
+				name :"z",
+				avatar:'',
+				content :XAMPP has been around for more than 10 years – there is a huge community behind it. You can get involved by joining our Forums.
+
+			},
+		];};
